95b54b65b8ccca927aaa8d04f326687fd25a28ad
Mario Julián Barón Rueda <mariobaron@gmail.com>
2021-07-06 09:55:18 -0500

    Fundamentos de React - Create React App (CRA)

---
+++ Proyectos/02-fundamentos/src/components/Contador.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const Contador = ({inicial, factor}) => {
+
+    const [contador, setContador] = useState(inicial);
+    const aumentar = () => {
+        setContador(contador + factor)
+
+    }
+
+    const disminuir = () => {
+        setContador(contador - factor)
+        
+    }
+
+    return (
+        <div>
+           <h1>Contador: {contador}</h1> 
+           <hr/>
+           <button onClick={aumentar}>Aumentar</button>
+           <button onClick={disminuir}>Disminuir</button>
+        </div>
+    )
+}
+
+export default Contador;
